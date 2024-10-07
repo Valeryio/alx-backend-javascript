95298eb22c53d29e3fbd3f64c83a52cb4fcb35d7
Valeryio <tommagoma23@gmail.com>
2024-10-07 14:43:18 +0000

    first update of the task 1

--- 0x00-ES6_basic/1-block-scoped.js
+++ 0x00-ES6_basic/1-block-scoped.js
@@ -1,12 +1,14 @@
 export default function taskBlock(trueOrFalse) {
-  var task = false;
-  var task2 = true;
+  let task = false;
+  let task2 = true;
 
   if (trueOrFalse) {
-    let task = true;
-    let task2 = false;
+    task = false;
+    task2 = true;
+
+  //  task = true;
+  //  task2 = false;
   }
 
   return [task, task2];
 }
-
