779795e3ac255cbbf213567c845159d1fefc2aa2
Valeryio <tommagoma23@gmail.com>
2024-10-08 10:13:46 +0000

    commit on the task 10

--- 0x00-ES6_basic/10-loops.js
+++ 0x00-ES6_basic/10-loops.js
@@ -1,8 +1,9 @@
 export default function appendToEachArrayValue(array, appendString) {
+  const newArray = [];
   for (const idx of array) {
-    const value = array[idx];
-    array[idx] = appendString + value;
+    const value = idx;
+    newArray.push(appendString + value);
   }
 
-  return array;
+  return newArray;
 }
