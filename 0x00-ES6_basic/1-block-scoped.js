35907cbebdd0ab8f959a6dc8be4d075d2bb30672
Valeryio <tommagoma23@gmail.com>
2024-10-08 07:24:21 +0000

    commit the task 2

--- 0x00-ES6_basic/1-block-scoped.js
+++ 0x00-ES6_basic/1-block-scoped.js
@@ -1,13 +1,10 @@
 export default function taskBlock(trueOrFalse) {
-  let task = false;
-  let task2 = true;
+  const task = false;
+  const task2 = true;
 
   if (trueOrFalse) {
-    task = false;
-    task2 = true;
-
-  //  task = true;
-  //  task2 = false;
+    const task = false;
+    const task2 = true;
   }
 
   return [task, task2];
