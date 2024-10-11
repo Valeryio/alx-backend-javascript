44065162e610b3bbc487a6c4378ce6d80dd2702a
Valeryio <tommagoma23@gmail.com>
2024-10-11 15:10:09 +0000

    Update on the task 3

--- 0x01-ES6_promise/3-all.js
+++ 0x01-ES6_promise/3-all.js
@@ -3,7 +3,7 @@ import { uploadPhoto, createUser } from './utils';
 export default async function handleProfileSignup() {
   return Promise.all([uploadPhoto(), createUser()])
     .then((result) => {
-      console.log(result[0].body + ' ' + result[1].firstName + ' ' + result[1].lastName);
+      console.log(result[0].body, result[1].firstName, result[1].lastName);
     })
     .catch(() => console.log('Signup system offline'));
 }
