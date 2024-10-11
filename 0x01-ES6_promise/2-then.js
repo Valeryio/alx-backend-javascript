a13e9e7f3f014e8b3a7fa5c3f49fe641406738ce
Valeryio <tommagoma23@gmail.com>
2024-10-11 14:07:48 +0000

    Commit on the task 2

--- 0x01-ES6_promise/2-then.js
+++ 0x01-ES6_promise/2-then.js
@@ -1,12 +1,14 @@
-
 export default function handleResponseFromAPI(promise) {
   promise.then(() => {
-      const response = {
-        status: 200,
-	body: 'Success'
-      };
-      resolve(response);
-    }).catch((error) => {
-      console.log("L'erreur est : ", error);
+    const response = {
+      status: 200,
+      body: 'success',
+    };
+    console.log('Got a response from the API');
+    return (response);
+  })
+    .catch(() => {
+      console.log('Got a response from the API');
+      return new Error('');
     });
 }
