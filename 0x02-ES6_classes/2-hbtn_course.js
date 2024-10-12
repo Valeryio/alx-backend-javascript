c4e8f9b949753afd8aa9e1fe3f187252c23b45ec
Valeryio <tommagoma23@gmail.com>
2024-10-12 11:58:36 +0000

    update on the task 2

--- 0x02-ES6_classes/2-hbtn_course.js
+++ 0x02-ES6_classes/2-hbtn_course.js
@@ -1,8 +1,20 @@
 export default class HolbertonCourse {
   constructor(name, length, students) {
-    this._name = name;
-    this._length = length;
-    this._students = students;
+    if (typeof name === 'string') {
+      this._name = name;
+    } else {
+      throw new Error('TypeError: Name must be a string');
+    }
+
+    if (typeof length === 'number') {
+      this._length = length;
+    } else {  
+      throw new Error('TypeError: Length must be a string');
+    }
+    
+    if (Array.isArray(students)) {
+      this._students = students;
+    }
   }
 
   get name() {
