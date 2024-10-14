76a06852cecc426886a25c9bf7ac0767fcddcb2b
Valeryio <tommagoma23@gmail.com>
2024-10-14 14:42:22 +0000

    update the task 10

--- 0x03-ES6_data_manipulation/10-update_uniq_items.js
+++ 0x03-ES6_data_manipulation/10-update_uniq_items.js
@@ -1,5 +1,9 @@
 export default function updateUniqueItems(map) {
   const newMap = map;
+  if (!(map instanceof Map)) {
+    throw new Error('Cannot process');
+  }
+
   for (const [key, value] of newMap) {
     if (value === 1) {
       newMap.set(key, 100);
