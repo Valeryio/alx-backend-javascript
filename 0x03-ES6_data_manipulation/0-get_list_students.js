1238793c8745995468e85c1d542fac261ae906ec
Valeryio <tommagoma23@gmail.com>
2024-10-14 10:01:48 +0000

    commit the task 0

---
+++ 0x03-ES6_data_manipulation/0-get_list_students.js
@@ -0,0 +1,20 @@
+export default function getListStudent() {
+  const studentsList = [
+    {
+      id: 1,
+      firstName: 'Guillaume',
+      location: 'San Francisco',
+    },
+    {
+      id: 2,
+      firstName: 'James',
+      location: 'Columbia',
+    },
+    {
+      id: 5,
+      firstName: 'Serena',
+      location: 'San Francisco',
+    },
+  ];
+  return studentsList;
+}
