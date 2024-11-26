9b89e91b6c8da46d992ab1e5fa6539d7ad3bf021
Valeryio <tommagoma23@gmail.com>
2024-11-26 09:59:37 +0000

    Update the task 1

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -8,6 +8,6 @@ const rl = readline.createInterface({
 rl.question('Welcome to Holberton School, what is your name?\n', (answer) => {
   console.log(`Your name is: ${answer}`);
 
-  console.log('This important software is now closing');
+  // console.log('This important software is now closing');
   rl.close();
 });
