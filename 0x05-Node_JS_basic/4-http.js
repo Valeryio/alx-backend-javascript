f072cf58b3ca34350a8a7499e41b536e0cff4e2c
Valeryio <tommagoma23@gmail.com>
2024-11-26 11:42:39 +0000

    Update the task 4

--- 0x05-Node_JS_basic/4-http.js
+++ 0x05-Node_JS_basic/4-http.js
@@ -1,14 +1,15 @@
 const http = require('http');
+
 const port = 1245;
 const hostname = '127.0.0.1';
 
 const requestHandler = (request, response) => {
   response.end('Hello Holberton School!');
-}
+};
 
 const app = http.createServer(requestHandler);
 app.listen(port, hostname, () => {
-    console.log(`Server running at http://${hostname}:${port}`);
+  console.log(`Server running at http://${hostname}:${port}`);
 });
 
 module.exports = app;
