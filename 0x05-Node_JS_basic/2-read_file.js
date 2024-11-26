8bc2660f31861c6c847b2a7d00d8d42986243f1b
Valeryio <tommagoma23@gmail.com>
2024-11-26 09:39:02 +0000

    Update the task 2

--- 0x05-Node_JS_basic/2-read_file.js
+++ 0x05-Node_JS_basic/2-read_file.js
@@ -1,49 +1,48 @@
-
-const fs = require("node:fs");
+const fs = require('node:fs');
 
 function countStudents(path) {
-  fs.readFile(path, "utf8", (err, data) => {
+  fs.readFile(path, 'utf8', (err, data) => {
     if (err) {
-      throw new Error("Cannot load the database");
+      throw new Error('Cannot load the database');
     }
-
-    result = data.split("\n");
+    const result = data.split('\n');
     result.pop();
 
     console.log(`Number of students: ${result.length - 1}`);
+    const fields = [];
 
-    fields = []
-    
-    for (let i = 0; i < result.length; i++){
-
+    for (let i = 0; i < result.length; i += 1) {
       // Get an array of data to manipulate each student
-      result[i] = result[i].split(",");
+      result[i] = result[i].split(',');
 
       // Get the different fields
       if (!fields.includes(result[i][3]) && i > 0) {
         fields.push(result[i][3]);
       }
     }
+    // console.log(result);
+    // console.log(fields);
 
-    console.log(result);
-    console.log(fields);
+    const fieldsObj = [];
 
-    fields_obj = [];
+    for (let i = 0; i < fields.length; i += 1) {
+      fieldsObj[i] = {};
+      fieldsObj[i].field = fields[i];
+      fieldsObj[i].number = 0;
+      fieldsObj[i].students = [];
 
-    for (let i = 1; i < fields.length; i++) {
-
-      let newObj = {};
-
-      for (let j = 1; j < result.length; j++) {
-
-        if (result[j][3] == fields[i]) {
-          newObj
+      for (let j = 1; j < result.length; j += 1) {
+        if (result[j][3] === fields[i]) {
+          fieldsObj[i].number += 1;
+          fieldsObj[i].students.push(result[j][0]);
         }
       }
+
+      fieldsObj[i].students = fieldsObj[i].students.join(', ');
+      console.log(`Number of students in ${fieldsObj[i].field}: ${fieldsObj[i].number}. List: ${fieldsObj[i].students}`);
     }
-
-
-    console.log(data);
+    // console.log(fieldsObj);
+    // console.log(data);
   });
 }
 
