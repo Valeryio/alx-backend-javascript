c2e8748be44dc9911f0c27fa1a0f76a5167b669f
Valeryio <tommagoma23@gmail.com>
2024-11-30 10:45:19 +0000

    update the task 1

--- 0x06-unittests_in_js/1-calcul.test.js
+++ 0x06-unittests_in_js/1-calcul.test.js
@@ -1,27 +1,31 @@
-import * as calcul from './1-calcul.js';
+// import * as calcul from './1-calcul.js';
 // import * as chai from 'chai';
-import assert from 'assert';
+// import assert from 'assert';
 
 // const assert = chai.assert;
 
+const calculateNumber = require('./1-calcul');
+const assert = require('assert');
+
+
 describe("calculateNumber", () => {
   it("SUM", function () {
-    let result = calcul.calculateNumber("SUM", 1.4, 4.5);
+    let result = calculateNumber("SUM", 1.4, 4.5);
     assert.equal(result, 6);
   })
 
   it("SUBTRACT", function () {
-    let result = calcul.calculateNumber("SUBTRACT", 1.4, 4.5);
+    let result = calculateNumber("SUBTRACT", 1.4, 4.5);
     assert.equal(result, -4);
   })
 
   it("DIVIDE", function () {
-    let result = calcul.calculateNumber("DIVIDE", 1.4, 4.5);
+    let result = calculateNumber("DIVIDE", 1.4, 4.5);
     assert.equal(result, 0.2);
   })
 
   it("DIVIDE", function () {
-    let result = calcul.calculateNumber("DIVIDE", 1.4, 0);
+    let result = calculateNumber("DIVIDE", 1.4, 0);
     assert.equal(result, "Error");
   })
 });
