de8f1e5680f81b56d337547aa938d2104e8c6108
Valeryio <tommagoma23@gmail.com>
2024-11-30 13:28:40 +0000

    update the task 2

--- 0x06-unittests_in_js/3-payment.test.js
+++ 0x06-unittests_in_js/3-payment.test.js
@@ -4,21 +4,35 @@
 
 const sinon = require('sinon');
 const Utils = require('./utils');
+const expect = require('chai').expect;
 const sendPaymentRequestToApi = require('./3-payment');
 
 
+describe('sendPaymentRequestToApi', () => {
+    it('it should call Utils.calculateNUmber with correct arguments', () => {
+        const utils = Utils;
+        const utilSpy = sinon.spy(utils, 'calculateNumber');
 
-const utils = Utils;
-const utilSpy = sinon.spy(utils, 'calculateNumber');
+        sendPaymentRequestToApi(10, 3);
+        expect(utilSpy.calledOnce).to.be.true;
+        expect(utilSpy.callCount).to.be.equal(1);
+
+        // Restore the original method
+        utilSpy.restore();
+    });
+
+    it('verify if calculateNumber is called with the right args', () => {
+        const utils = Utils;
+        const utilSpy = sinon.spy(utils, 'calculateNumber');
+
+        sendPaymentRequestToApi(10, 3);
+        expect(utilSpy.calledWith("SUM", 10, 3)).to.be.true;
+
+        // Restore the original method
+        utilSpy.restore();
+    })
+})
 
 
-sendPaymentRequestToApi(10, 3);
+// expect(utilSpy.called).to.be.true;
 
-
-console.log(utilSpy.called);
-console.log(utilSpy.callCount);
-
-
-
-// Restore the original method
-utilSpy.restore();
